2312a89b5ab2dbe8709625475134b02bceee3efb
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2025-05-07 13:57:20 -0700

    add news

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    domain: 'http://192.168.0.213:8000' // Remplacez par votre domaine réel
+    domain: 'https://192.168.0.213:8000' // Remplacez par votre domaine réel
   };
