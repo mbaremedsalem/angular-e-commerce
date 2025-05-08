22039bfe71eaf000db32c4a819537a518549d526
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2025-05-08 10:57:51 -0700

    change urls

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    domain: 'https://192.168.0.213:8000' // Remplacez par votre domaine réel
+    domain: 'https://192.168.0.207:8000' // Remplacez par votre domaine réel
   };
